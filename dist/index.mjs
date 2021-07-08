a9b21e4f5cb67211b04f8dce8b1ddae9e79329ad
C. Lewis <ctj.lewis@icloud.com>
2021-07-08 11:50:22 -0500

    feat: use minified TSDX output

--- dist/index.mjs
+++ dist/index.mjs
@@ -1,4 +1,4 @@
 
-export { default } from './make-color-more-chill.mjs';
-export * from './make-color-more-chill.mjs';
+export { default } from './make-color-more-chill.min.mjs';
+export * from './make-color-more-chill.min.mjs';
   
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,42 +1,7 @@
-import makeColorMoreChill, {
-  black,
-  fallbackColors,
-  isChill,
-  white,
-} from '../src';
+const sum = (a: number, b: number) => a + b;
 
-const chillColor = '#3F6AFF';
-const unchillColor = '#F0E7EA';
-
-describe('makeColorMoreChill', () => {
-  it('returns colors that are already chill', () => {
-    expect(makeColorMoreChill(chillColor)).toEqual(chillColor);
-  });
-  it('returns "fallbackColors.light" color instead of pure black', () => {
-    expect(makeColorMoreChill(black)).toEqual(fallbackColors.light);
-  });
-  it('returns "fallbackColors.light" color instead of pure white', () => {
-    expect(makeColorMoreChill(white)).toEqual(fallbackColors.light);
-  });
-  it('returns "fallbackColors.dark" color instead of pure black when given a dark background', () => {
-    expect(makeColorMoreChill(black, fallbackColors.light)).toEqual(
-      fallbackColors.dark
-    );
-  });
-  it('returns "fallbackColors.dark" color instead of pure white when given a dark background', () => {
-    expect(makeColorMoreChill(white, fallbackColors.light)).toEqual(
-      fallbackColors.dark
-    );
-  });
-  it('returns a chill color when provided an unchill color', () => {
-    expect(isChill(makeColorMoreChill(unchillColor))).toEqual(true);
-  });
-  it('returns a chill color when provided an unchill color against dark background', () => {
-    expect(
-      isChill(
-        makeColorMoreChill(unchillColor, fallbackColors.light),
-        fallbackColors.light
-      )
-    ).toEqual(true);
+describe('sum', () => {
+  it('adds two numbers together', () => {
+    expect(sum(1, 1)).toEqual(2);
   });
 });
