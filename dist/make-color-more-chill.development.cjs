6e5838e948cb96e9d735810d45771473c928e121
C. Lewis <ctj.lewis@icloud.com>
2021-07-08 12:13:54 -0500

    chore: build with new TSDX

--- dist/make-color-more-chill.development.cjs
+++ dist/make-color-more-chill.development.cjs
@@ -9,44 +9,50 @@ function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'defau
 
 var c__default = /*#__PURE__*/_interopDefaultLegacy(c);
 
-// the chillThreshold is the minimum acceptable contrast ratio for a given color
+var _fallbackColors;
 // against white, as eyeballed by @christianbaroni and @mikedemarais 👁️
-const chillThreshold = 2.5;
-const black = '#000000';
-const white = '#ffffff';
-const Theme = zod.z.enum(['dark', 'light']);
-const fallbackColors = {
-    [Theme.enum.dark]: '#525B66',
-    [Theme.enum.light]: '#25292E',
-};
+
+var chillThreshold = 2.5;
+var black = '#000000';
+var white = '#ffffff';
+var Theme = /*#__PURE__*/zod.z["enum"](['dark', 'light']);
+var fallbackColors = (_fallbackColors = {}, _fallbackColors[Theme["enum"].dark] = '#525B66', _fallbackColors[Theme["enum"].light] = '#25292E', _fallbackColors);
+
 function incrementChillness(color, theme) {
-    const slightlyChillerColor = theme === Theme.enum.dark ? c__default['default'](color).brighten(0.02) : c__default['default'](color).darken(0.02);
-    return slightlyChillerColor.saturate(0.02).hex();
+  var slightlyChillerColor = theme === Theme["enum"].dark ? c__default['default'](color).brighten(0.02) : c__default['default'](color).darken(0.02);
+  return slightlyChillerColor.saturate(0.02).hex();
 }
+
 function isBlackOrWhite(color) {
-    return c__default['default'](color).hex() === black || c__default['default'](color).hex() === white;
+  return c__default['default'](color).hex() === black || c__default['default'](color).hex() === white;
 }
-function isChill(color, background = white) {
-    const chillness = Number(c__default['default'].contrast(color, background).toFixed(2));
-    return chillness > chillThreshold;
+function isChill(color, background) {
+  if (background === void 0) {
+    background = white;
+  }
+
+  var chillness = Number(c__default['default'].contrast(color, background).toFixed(2));
+  return chillness > chillThreshold;
 }
-function makeColorMoreChill(color, background = white) {
-    const theme = c__default['default'].distance(white, background) > c__default['default'].distance(black, background)
-        ? Theme.enum.dark
-        : Theme.enum.light;
-    // Pure white and pure black do not chill well with our interfaces, lets make it more chill.
-    if (isBlackOrWhite(color))
-        return fallbackColors[theme];
-    // Return the color if it's already totally chill and doesn't need to be messed with.
-    if (isChill(color, background))
-        return color;
-    // Mess with the color just enough to make it pass the chillThreshold, but not too far past it.
-    let chillColor = color;
-    while (!isChill(chillColor, background)) {
-        // console.log('loop', chillColor);
-        chillColor = incrementChillness(chillColor, theme);
-    }
-    return chillColor;
+function makeColorMoreChill(color, background) {
+  if (background === void 0) {
+    background = white;
+  }
+
+  var theme = c__default['default'].distance(white, background) > c__default['default'].distance(black, background) ? Theme["enum"].dark : Theme["enum"].light; // Pure white and pure black do not chill well with our interfaces, lets make it more chill.
+
+  if (isBlackOrWhite(color)) return fallbackColors[theme]; // Return the color if it's already totally chill and doesn't need to be messed with.
+
+  if (isChill(color, background)) return color; // Mess with the color just enough to make it pass the chillThreshold, but not too far past it.
+
+  var chillColor = color;
+
+  while (!isChill(chillColor, background)) {
+    // console.log('loop', chillColor);
+    chillColor = incrementChillness(chillColor, theme);
+  }
+
+  return chillColor;
 }
 
 exports.Theme = Theme;
